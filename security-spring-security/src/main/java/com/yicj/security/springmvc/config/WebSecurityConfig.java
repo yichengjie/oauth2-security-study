ec600217d4c2277520fd1929f21a5c0bdeba48a4
易成杰 <626659321@qq.com>
2020-07-14 09:23:59 +0800

    项目名称更新

---
+++ security-spring-security/src/main/java/com/yicj/security/springmvc/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.yicj.security.springmvc.config;
+
+/**
+ * ClassName: WebSecurityConfig
+ * Description: TODO(描述)
+ * Date: 2020/7/14 9:13
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class WebSecurityConfig {
+}
