700bb7c4ba6c1bc509717f27fd8a391e47183ce8
易成杰 <626659321@qq.com>
2020-07-14 21:28:28 +0800

    授权服务工程

--- distributed‐security/distributed-security-uaa/src/main/java/com/yicj/distributed/uua/UAAServer.java
+++ distributed‐security/distributed-security-uaa/src/main/java/com/yicj/distributed/uua/UAAServer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * ClassName: UAAServer
@@ -17,8 +18,8 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"com.yicj.distributed.uua"})
 public class UAAServer {
-
     public static void main(String[] args) {
         SpringApplication.run(UAAServer.class, args) ;
     }
