82cd206a066a40b2e763f8a8967a859aa42ef1f7
易成杰 <626659321@qq.com>
2020-07-15 10:12:32 +0800

    注册中心搭建

---
+++ distributed-security3/distributed-security-discovery/src/main/java/com/yicj/distributed/discovery/DiscoveryServer.java
@@ -0,0 +1,23 @@
+package com.yicj.distributed.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * ClassName: com.yicj.distributed.discovery.DiscoveryServer
+ * Description: TODO(描述)
+ * Date: 2020/7/15 10:09
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServer.class, args) ;
+    }
+}
